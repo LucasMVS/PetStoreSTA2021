144f19dc2bf8d641065fa874e529d0206cf56135
Lucas Martins <Lucasmvs2001@hotmail.com>
2021-08-12 23:47:46 -0300

    Completei o script incluirPet (Post)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 public class Pet {
     String uri = "https://petstore.swagger.io/v2/pet";
@@ -31,6 +32,8 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Hatchi"))
                 .body("status", is("available"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
         ;
     }
 
