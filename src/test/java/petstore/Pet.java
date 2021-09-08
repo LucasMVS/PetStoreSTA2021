b6d25d75447b56fe141646353b91dbf5a2953300
Lucas Martins <Lucasmvs2001@hotmail.com>
2021-09-08 16:41:29 -0300

    Finalizando o script excluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -37,29 +37,61 @@ public class Pet {
         ;
     }
 
-    @Test(priority = 2)
-    public void consultarPet(){
+    @Test(priority=2)
+    public void consultarPet() {
         String petId = "1147877";
 
-
         String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                .when()
+                        .get(uri + "/" + petId)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Hatchi"))
+                        .body("category.name", is("AX2345LORT"))
+                        .body("status", is("available"))
+                .extract()
+                        .path("category.name");
+        System.out.println("O token é " + token);
+    }
+
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
         given()
                 .contentType("application/json")
                 .log().all()
+                .body(jsonBody)
         .when()
-                .get(uri + "/" + petId)
+                .put(uri)
         .then()
                 .log().all()
                 .statusCode(200)
                 .body("name", is("Hatchi"))
-                .body("category.name", is("AX2345LORT"))
-                .body("status", is("available"))
-        .extract()
-                .path("category.name")
+                .body("status", is("sold"))
+    ;
+    }
+
+    @Test(priority = 4)
+    public void excluirPet() {
+        String petId = "1147877";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
         ;
-
-        System.out.println("O token é: " + token);
-
     }
 
 }
